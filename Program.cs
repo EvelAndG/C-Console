166a4121dddce9cfdef62c757275308a751257bb
Gage <gw_eveland@hotmail.com>
2022-01-31 15:53:55 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        Console.WriteLine("Hello World!");
+        int userInt = 1;
+
+        while (userInt != 7)
+        {
+
+            Console.WriteLine("Menu here: ");
+
+            Console.WriteLine("Make a selection: ");
+            string userInp = Console.ReadLine();
+            userInt = Convert.ToInt32(userInp);
+
+        if (userInt ==1)
+        {
+            Console.WriteLine("Yep, it's One");
+        }
+        }
+
+
+
+        }
+    }
+}
