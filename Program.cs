2bd4a9a78cbffd6879cc079de21c923c34956e9b
Gage <gw_eveland@hotmail.com>
2022-02-01 09:35:33 -0600

    Add Part 1 and Exit Console

--- Program.cs
+++ Program.cs
@@ -6,20 +6,28 @@
         {
             
         Console.WriteLine("Hello World!");
-        int userInt = 1;
+        int userInt = 0;
 
         while (userInt != 7)
         {
 
-            Console.WriteLine("Menu here: ");
+            Console.WriteLine("Enter a number to make a selection: \n 1.) Greet by name \n 2.) Calculate area of rectangle \n 3.) Calculate MPG \n 4.) String search \n 5.)Close application");
 
-            Console.WriteLine("Make a selection: ");
+            
             string userInp = Console.ReadLine();
             userInt = Convert.ToInt32(userInp);
 
         if (userInt ==1)
         {
-            Console.WriteLine("Yep, it's One");
+            Console.WriteLine("What is your name? ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello, "+userName);
+            Console.WriteLine(" ");
+        }
+
+        if (userInt == 5)
+        {
+        Environment.Exit(0);
         }
         }
 
